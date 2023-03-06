7b2902ea34b3a7d30c4dfd3bfcec010e55f1685f
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-03-06 07:07:10 +0000

    Initial commit.

--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -2,34 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { PokemonService } from 'src/app/services/pokemon-catalogue.service';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.css']
+  styleUrls: ['./collection.component.css'],
 })
-export class CollectionComponent implements OnInit{
+export class CollectionComponent implements OnInit {
 
   get user(): User | undefined {
     return this.userService.user;
   }
 
-  /*
-  get captureds(): Pokemon[] {
-    if (this.userService.user) {
-      return this.userService.user.favouritesPokemon;
-    }
-    return [];
-  }
-  */
-
   get captureds1(): Pokemon[] {
     return this.userService.getCapturedPokemon();
   }
 
-  constructor (
+  constructor(
     private userService: UserService,
-  ) { }
+    private pokemonService: PokemonService
+  ) {}
 
   ngOnInit(): void {
   }
